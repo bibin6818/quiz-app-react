498dcec25790834844fc900c22204021e26764e0
bibin6818 <bibinthomas5956@gmail.com>
2024-05-03 21:52:43 +0530

    :completed

---
+++ src/assets/components/QuizResult.jsx
@@ -0,0 +1,14 @@
+
+function QuizResult(props) {
+  return (
+    <>
+    <div className='show-score'>
+        Your Score:{props.score}<br/>
+        Total Score:{props.totalScore}
+    </div>
+    <button id="next-button" onClick={props.tryAgain}>Try Again</button>
+    </>
+  )
+}
+
+export default QuizResult
